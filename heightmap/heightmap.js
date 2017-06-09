516dff6fc232b3a5314d8ff5ba02ddb72d4a10d1
bubblesama <paul.remignon@gmail.com>
2017-06-09 15:54:14 +0200

    [heightmap] click buggy

--- heightmap/heightmap.js
+++ heightmap/heightmap.js
@@ -1,5 +1,4 @@
 
-
 // x = horizontal screen cordinate from left
 // y = vertical screen cordinate from top
 // I (1,0,0) geo vector
@@ -8,8 +7,8 @@
 // M is (i,j,k) in geo and (x,y) in screen
 
 //base constants for screen projection
-var x0 = 0;
-var y0 = 200;
+var x0 = 60;
+var y0 = 300;
 var xI = 30;
 var yI = 20;
 var xJ = 30;
@@ -17,39 +16,58 @@ var yJ = -20;
 var xK = 0;
 var yK = -10;
 
+
+var normScreenI = Math.sqrt(xI*xI+yI*yI);
+var normScreenJ = Math.sqrt(xJ*xJ+yJ*yJ);
+
 var oddFillColor = "lightblue";
 var evenFillColor = "red";
+
 
 
 
 //matrix of k index for heightmap: k of M(i,j,k)= kIndex[i][j]
 
 var kIndex = [
-	[0, 0, 0, 1, 1, 0],
-	[0, 0, 2, 2, 2, 1],
-	[0, 1, 2, 5, 2, 1],
-	[0, 1, 2, 2, 2, 1],
-	[0, 0, 1, 1, 1, 0],
-	[0, 0, 0, 0, 0, 0]
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0],
+	[0, 0, 0, 0, 0, 5, 5, 4, 8, 0, 0, 0],
+	[0, 0, 0, 0, 0, 5, 3, 0, 5, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ];
 
+var maxI = kIndex.length;
+var maxJ = kIndex[0].length;
 
+
+
+var canvas = document.getElementById("mainCanvas");
+var context = canvas.getContext("2d");
+
+window.addEventListener('mouseclick', getMousePos, false);
 
 function launchCanvas(){
-	var canvas = document.getElementById("mainCanvas");
-	var context = canvas.getContext("2d");
+
+	context.fillStyle = "lightblue";
+	context.fillRect(10,10,790,590);
 	context.strokeStyle = "black"; 
-	context.lineWidth = 0; 
-	for (var i=0;i<5;i++){
-		for (var j=5;j>=0;j--){
-		
-			//'rgb(240, 240, 0)'
-		
+	context.lineWidth = 1; 
+	//heightmap
+	for (var i=0;i<maxI-1;i++){
+		for (var j=maxJ-1;j>=0;j--){
+			//color
 			var colorR = ((i+j)%2)== 0?'250':'0';
-			//var colorGVal = i*100;
 			var colorGVal = 100;
 			var colorG = ''+colorGVal;
 			var colorB = ((i+j)%2)== 0?'0':'250';
+			//trace
 			context.fillStyle = 'rgb('+colorR+', '+colorG+', '+colorB+')';
 			context.beginPath();
 			context.moveTo(x0+(i+0)*xI+(j+0)*xJ+kIndex[i+0][j+0]*xK,y0+(i+0)*yI+(j+0)*yJ+kIndex[i+0][j+0]*yK);   
@@ -58,15 +76,42 @@ function launchCanvas(){
 			context.lineTo(x0+(i+0)*xI+(j+1)*xJ+kIndex[i+0][j+1]*xK,y0+(i+0)*yI+(j+1)*yJ+kIndex[i+0][j+1]*yK);   
 			context.closePath();
 			context.fill();
-			//context.stroke();
+			context.stroke();
 		}
 	}
-	
-	
-
-	
-	
-	
-	
 };
 
+
+canvas.addEventListener(
+	'click',
+	function(evt) {
+        var mousePos = getMousePos(canvas, evt);
+		document.getElementById("mouseX").innerHTML = Math.floor(mousePos.x);
+		document.getElementById("mouseY").innerHTML = Math.floor(mousePos.y);
+		var xM = Math.floor(mousePos.x-x0);
+		var yM = Math.floor(mousePos.y-y0);
+		var iM = Math.floor(((xI*xM)+(yI*yM)) /(normScreenI*normScreenI));
+		var jM = Math.floor(((xJ*xM)+(yJ*yM)) /(normScreenJ*normScreenJ));
+		console.log("click: xM="+xM+" yM="+yM+" iM="+iM);
+		document.getElementById("mouseI").innerHTML = iM;
+		document.getElementById("mouseJ").innerHTML = jM;
+		
+		if (iM >=0 && iM<maxI && jM >= 0 && jM < maxJ){
+			kIndex[iM+0][jM+0]--;
+			kIndex[iM+1][jM+0]--;
+			kIndex[iM+0][jM+1]--;
+			kIndex[iM+1][jM+1]--;
+			launchCanvas();
+		}
+		
+		
+		
+	}
+);
+
+function getMousePos(canvas, evt){
+	var rect = canvas.getBoundingClientRect();
+	var x = (evt.clientX - rect.left) / (rect.right - rect.left) * canvas.width;
+	var y = (evt.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height;
+	return {"x": x, "y":y}
+};
