4603834c647e7d67e2caa4e51f49c1473dc4ea65
bb <paul.remignon@gmail.com>
2017-07-15 13:17:06 +0200

    [dd] init js

---
+++ dd-cm/dd-cm.js
@@ -0,0 +1,16 @@
+
+
+
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+
+function start(){
+	console.log("start - IN");
+	context.fillStyle = "rgb(38,127,0)";
+	context.fillRect(0,0,800,600);
+	
+	//new Color(117, 204, 128);
+	
+	
+	
+};
