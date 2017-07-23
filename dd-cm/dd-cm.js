e07e8545536f4b0adcfb9c6676d0bd409986e475
bubblesama <paul.remignon@gmail.com>
2017-07-23 23:09:58 +0200

    [dd] init: integration sprites

--- dd-cm/dd-cm.js
+++ dd-cm/dd-cm.js
@@ -7,10 +7,11 @@ var context = canvas.getContext("2d");
 function start(){
 	console.log("start - IN");
 	context.fillStyle = "rgb(38,127,0)";
+	context.imageSmoothingEnabled = false;
 	context.fillRect(0,0,800,600);
-	
 	//new Color(117, 204, 128);
-	
-	
-	
+	var worldSprites = new Image();
+	worldSprites.src = "dd-world.png";
+	context.drawImage(worldSprites,8,16,8,8,30,60,32,32);
+
 };
