a9a7dafe3ad264422c664dd6f9a9c885365ab868
bubblesama <paul.remignon@gmail.com>
2017-07-25 23:02:19 +0200

    [dd] boucle sur monde pour sprites

--- dd-cm/dd-cm.js
+++ dd-cm/dd-cm.js
@@ -4,6 +4,14 @@
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 
+
+const worldHeightByTile = 11;
+const worldWidthByTile = 19;
+const worldTileHeight = 8;
+const worldTileWidth = 8;
+
+const worldZoom = 4;
+
 function start(){
 	console.log("start - IN");
 	context.fillStyle = "rgb(38,127,0)";
@@ -12,6 +20,10 @@ function start(){
 	//new Color(117, 204, 128);
 	var worldSprites = new Image();
 	worldSprites.src = "dd-world.png";
-	context.drawImage(worldSprites,8,16,8,8,30,60,32,32);
-
+	
+	for (var i=0;i<worldWidthByTile;i++){
+		for (var j=0;j<worldHeightByTile;j++){
+			context.drawImage(worldSprites,8,16,worldTileWidth,worldTileHeight,30+i*worldTileWidth*worldZoom,60+j*worldTileHeight*worldZoom,worldZoom*worldTileWidth,worldTileHeight*worldZoom);
+		}
+	}
 };
