b94b8af716f50937a3366a8b6dfcdf7510810b7c
bubblesama <paul.remignon@gmail.com>
2017-07-28 15:12:42 +0200

    [dd] static graphical display of the worldmap

--- dd-cm/dd-cm.js
+++ dd-cm/dd-cm.js
@@ -15,18 +15,18 @@ var worldSpritesCoordinatesByName = {
 	"RIVER_DOWN_LEFT": [2,2],
 	"RIVER_LEFT_UP": [3,2],
 	"MOUTAIN_BLACK": [0,0],
-	"MOUTAIN_GREY": [7,0],
-	"MOUTAIN_BLUE": [7,0],
-	"MOUTAIN_RED": [7,0],
-	"MOUTAIN_PURPLE": [7,0],
-	"MOUTAIN_BLANK": [7,0],
-	"MOUTAIN_BIG": [7,0],
-	"WALL_DOOR_UP_DOWN": [7,0],
-	"WALL_DOOR_LEFT_RIGHT": [7,0],
-	"WALL_UP_DOWN": [7,0],
-	"WALL_LEFT_RIGHT": [7,0],
-	"FOREST": [7,0],
-	"HOUSE": [7,0]
+	"MOUTAIN_GREY": [2,0],
+	"MOUTAIN_BLUE": [3,0],
+	"MOUTAIN_RED": [4,0],
+	"MOUTAIN_PURPLE": [5,0],
+	"MOUTAIN_BLANK": [1,0],
+	"MOUTAIN_BIG": [5,1],
+	"WALL_DOOR_UP_DOWN": [0,3],
+	"WALL_DOOR_LEFT_RIGHT": [1,3],
+	"WALL_UP_DOWN": [3,3],
+	"WALL_LEFT_RIGHT": [2,3],
+	"FOREST": [0,1],
+	"HOUSE": [2,1]
 };
 
 var worldTileByLetter = {
@@ -53,35 +53,35 @@ var worldTileByLetter = {
 var mapFileName = "";
 
 function start(){
-	console.log("start - IN");
+	//console.log("start - IN");
 	context.fillStyle = "rgb(117,204,128)";
 	context.imageSmoothingEnabled = false;
 	context.fillRect(0,0,618,490);
 	var worldSprites = new Image();
 	worldSprites.src = "dd-world.png";
-
-	
-	for (var i=0;i<worldWidthByTile;i++){
-		for (var j=0;j<worldHeightByTile;j++){
-			var character = worldMapData.tiles.charAt(j*19+i);
-			console.log("start: char=\""+character+"\"");
-			var worldTileName = worldTileByLetter[""+character];
-			console.log("start: char=\""+character+"\" worldTileName="+worldTileName);
-			var spriteToShowCoordinates = worldSpritesCoordinatesByName[""+worldTileName];
-			console.log("start: char=\""+character+"\" worldTileName="+worldTileName+" spriteToShowCoordinates="+spriteToShowCoordinates[0]+","+spriteToShowCoordinates[1]);
-			context.drawImage(
-				worldSprites,
-				spriteToShowCoordinates[0]*worldTileWidth,
-				spriteToShowCoordinates[1]*worldTileHeight,
-				worldTileWidth,
-				worldTileHeight,
-				5+i*worldTileWidth*worldZoom,
-				74+j*worldTileHeight*worldZoom,
-				worldZoom*worldTileWidth,
-				worldTileHeight*worldZoom
-			);
+	worldSprites.onload = function(){
+		for (var i=0;i<worldWidthByTile;i++){
+			for (var j=0;j<worldHeightByTile;j++){
+				var character = worldMapData.tiles.charAt(j*19+i);
+				//console.log("start: char=\""+character+"\"");
+				var worldTileName = worldTileByLetter[""+character];
+				//console.log("start: char=\""+character+"\" worldTileName="+worldTileName);
+				var spriteToShowCoordinates = worldSpritesCoordinatesByName[""+worldTileName];
+				//console.log("start: char=\""+character+"\" worldTileName="+worldTileName+" spriteToShowCoordinates="+spriteToShowCoordinates[0]+","+spriteToShowCoordinates[1]);
+				context.drawImage(
+					worldSprites,
+					spriteToShowCoordinates[0]*worldTileWidth,
+					spriteToShowCoordinates[1]*worldTileHeight,
+					worldTileWidth,
+					worldTileHeight,
+					5+i*worldTileWidth*worldZoom,
+					74+j*worldTileHeight*worldZoom,
+					worldZoom*worldTileWidth,
+					worldTileHeight*worldZoom
+				);
+			}
 		}
-	}
-	
+	};
+
 };
 
