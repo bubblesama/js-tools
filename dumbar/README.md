4222878164be10a0ef7dc10b467a95870426f496
john_larick <john.larick@gmail.com>
2019-10-10 17:45:18 +0200

    dumbar: skeletton for create person

--- dumbar/README.md
+++ dumbar/README.md
@@ -3,3 +3,17 @@ since autumn 2019
 
 concept
 from https://en.wikipedia.org/wiki/Dunbar%27s_number, a tool to check how a well-functioning social human you are, by tracking yout interactions with your friends and family
+
+
+package
+npm install express --save 
+npm install lokijs --save 
+
+
+TODO
+listing de personnes: nom (d'une personne ou d'un groupe)
+creation de personnes
+souvenir: type, date, remarques
+planification: à détailler
+
+
--- dumbar/launcher.js
+++ dumbar/launcher.js
@@ -1,45 +1,107 @@
-// INIT SERVER
+// - INIT SERVER ---------------------------------------------------------
 var express = require('express');
 var hostname = 'localhost';
 var port = 8000;
 var app = express();
+app.use(express.urlencoded());
 var router = express.Router()
+// init loki
+var loki = require('lokijs');
+var database = new loki(
+    'dumbar-lokidb.json',
+    {
+        autosave: "true",
+        autosaveInterval: 2000,
+    }
+);
+// - /INIT SERVER --------------------------------------------------------
 
-// ROUTING
 
+// - ROUTING --------------------------------------------------------------
 router.route("/")
 .get(function(request, response){
     _displayDefault(request,response);
 });
-
-
-/*
-router.route("/dumbar/persons")
-.get(_listAllPersons);
-*/
-
 router.route("/dumbar/persons")
 .get(function(request, response){
     _listAllPersons(request,response);
+}).post(function(request, response){
+    _createPerson(request,response);
 });
+router.route("/dumbar/web/")
+.get(function(request, response){
+    _displayGui(request,response);
+});
+// - /ROUTING -------------------------------------------------------------
 
 
-// - BUSINESS CODE -----------------------------------------------------------
-
+// - CONTROLLER -----------------------------------------------------------
 var _displayDefault = function(request, response){
     response.writeHead(200, {"Content-Type": "text/html"});
-    response.end("<html><head><title>dumbar</title></head><body><h1>dumbar</h1><p>todo, <a href='/dumbar/persons'>all</a></p></body></html>");
+    response.end("<html><head><title>dumbar</title></head><body><h1>dumbar</h1>"+
+    "<p><a href='/dumbar/persons'>all</a></p>"+
+    "<p><a href='/dumbar/web/'>GUI</a></p>"+
+    "</body></html>");
 };
-
-
+var _displayGui = function(request, response){
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.end(
+        "<html><head><title>dumbar</title></head>"+
+        "<body><h1>dumbar - add person</h1>"+
+        "<form action='/dumbar/persons' method='post'>"+
+        "<p>name: <input type='text' name='name'></p>"+
+        "<p><input type='submit' value='create'></p>"+
+        "</form>"+
+        "</body></html>"
+    );
+};
 var _listAllPersons = function(request, response){
-    response.json({message: "todo"});
+    response.json({message: "todo", persons: dbGetAllPersons()});
 };
+var _createPerson = function(request, response){
+    dbCreatePerson(request.body.name);
+    response.json({message: "done", name: request.body.name});
+};
+// - /CONTROLLER ----------------------------------------------------------
 
-// - /BUSINESS CODE ----------------------------------------------------------
 
-// LAUNCHING SERVER
-app.use(router); 
-app.listen(port, hostname, function(){
-	console.log("launching serveur on http://"+ hostname +":"+port); 
-});
+// - OBJETS ---------------------------------------------------------------
+class Person {
+	constructor(name){
+		this.name = name;
+	}
+};
+// - /OBJETS --------------------------------------------------------------
+
+
+// - DB -------------------------------------------------------------------
+var dbGetAllPersons = function(){
+    var personsDbCollection = database.addCollection("person");
+    var rawPersonList =  personsDbCollection.find();
+    var result = [];
+    //filter set parameters
+    for (var i=0;i<rawPersonList.length;i++){
+        result.push({name: rawPersonList[i].name});
+    }
+    return result;
+};
+var dbCreatePerson = function(name){
+    var personsDbCollection = database.addCollection("person");
+    personsDbCollection.insertOne({name: name});
+};
+// - /DB ------------------------------------------------------------------
+
+
+// - LAUNCHING SERVER -----------------------------------------------------
+app.use(router);
+database.loadDatabase(
+    {},
+    function(err){
+        var logDbCollection = database.addCollection("log");
+        //dbCreatePerson("John");
+        app.listen(port, hostname, function(){
+            console.log("launching serveur on http://"+ hostname +":"+port); 
+        });
+    }
+);
+// - /LAUNCHING SERVER ----------------------------------------------------
