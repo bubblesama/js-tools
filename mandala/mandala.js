fad1fce05c8db8418ad134855656ae151b78e061
john_larick <john.larick@gmail.com>
2020-02-20 14:52:38 +0100

    mandala: first circles

--- mandala/mandala.js
+++ mandala/mandala.js
@@ -61,13 +61,96 @@ var conf = {
 };
 
 var model = {
-	update: function(){}
+	glyphs: [],
+	update: function(){
+		for (var i=0;i<model.glyphs.length;i++){
+			var glyph = model.glyphs[i];
+			glyph.update();
+		}
+	},
+	addGlyph(glyph){
+		this.glyphs.push(glyph);
+	}
+};
+
+var getSimpleCircleGlyph = function(x0, y0, r){
+	return {
+		x0: x0,
+		y0: y0,
+		r: r,
+		update: function(){},
+		draw: function(context){
+			context.fillStyle = conf.display.palette.main;
+			fillEllipse(context,this.x0,this.y0,this.r,this.r);
+		}
+	};
+};
+
+var getSimpleBlinkingCircleGlyph = function(x0, y0, r){
+	return {
+		x0: x0,
+		y0: y0,
+		r: r,
+		maxTick: 10,
+		currentTick: 0,
+		visible: true,
+		update: function(){
+			this.currentTick++;
+			if (this.maxTick < this.currentTick){
+				this.currentTick = 0;
+				this.visible = !this.visible;
+			}
+		},
+		draw: function(context){
+			if (this.visible){
+				context.fillStyle = conf.display.palette.main;
+				fillEllipse(context,this.x0,this.y0,this.r,this.r);
+			}
+		}
+	};
+};
+
+var getRotatingCircleGlyph = function(x0, y0, wayR, r, steps, currentStep){
+	return {
+		steps: steps,
+		currentStep: currentStep,
+		x0: x0,
+		y0: y0,
+		r: r,
+		wayR: wayR,
+		update: function(){
+			this.currentStep++;
+			if (this.currentStep >= this.steps){
+				this.currentStep = 1;
+			}
+		},
+		draw: function(context){
+			context.fillStyle = conf.display.palette.main;
+			fillEllipse(
+				context,
+				this.x0+this.wayR*Math.cos(Math.PI*2.0*this.currentStep/this.steps),
+				this.y0+this.wayR*Math.sin(Math.PI*2.0*this.currentStep/this.steps),
+				this.r,
+				this.r);
+		}
+	};
 };
 
 var display = {
 	draw: function(){
+		// cleaning
 		context.fillStyle = conf.display.palette.darkest;
-		context.fillRect(0,0,conf.display.screen.w,conf.display.screen.h);
+		context.fillRect(
+			0,
+			0,
+			conf.display.screen.w,
+			conf.display.screen.h
+		);
+		// 
+		for (var i=0;i<model.glyphs.length;i++){
+			var glyph = model.glyphs[i];
+			glyph.draw(context);
+		}
 	}
 };
 
@@ -91,6 +174,18 @@ function fillEllipse(ctx, x, y, w, h) {
 
 //************************************ INIT ***********************************
 function start(){
+	model.addGlyph(getSimpleBlinkingCircleGlyph(50,30,20));
+	model.addGlyph(getSimpleCircleGlyph(60,30,100));
+	model.addGlyph(getSimpleCircleGlyph(200,300,40));
+	model.addGlyph(getRotatingCircleGlyph(200,300,100,10,400));
+	model.addGlyph(getRotatingCircleGlyph(200,300,100,10,400,1));
+	model.addGlyph(getRotatingCircleGlyph(200,300,100,10,400, 50));
+	model.addGlyph(getRotatingCircleGlyph(200,300,100,10,400,100));
+	model.addGlyph(getRotatingCircleGlyph(200,300,100,10,400,150));
+	model.addGlyph(getRotatingCircleGlyph(200,300,100,10,400,200));
+	model.addGlyph(getRotatingCircleGlyph(200,300,100,10,400,250));
+	model.addGlyph(getRotatingCircleGlyph(200,300,100,10,400,300));
+	model.addGlyph(getRotatingCircleGlyph(200,300,100,10,400,350));
 	requestAnimationFrame(mainLoop);
 };
 
@@ -99,3 +194,6 @@ function mainLoop() {
 	display.draw();
 	requestAnimationFrame(mainLoop);
 };
+
+
+
