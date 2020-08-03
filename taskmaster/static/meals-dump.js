bbf04671801bc36377ceb5fe8ecf99c1fc044ea2
john_larick <john.larick@gmail.com>
2020-08-03 18:19:16 +0200

    meals: client side regexp check for dump

--- taskmaster/static/meals-dump.js
+++ taskmaster/static/meals-dump.js
@@ -1,5 +1,28 @@
 document.getElementById("dump-check-button").onclick = checkDump;
+document.getElementById("dump").oninput = resetCheck;
 
 function checkDump(){
-	document.getElementById("dump-check-status").innerHTML = "check!";
+	var rawDump = document.getElementById("dump").value;
+	var dumpLines = rawDump.replace(/\r\n/g,"\n").split("\n");
+	var ok = true;
+	for (var i=0;i<dumpLines.length;i++){
+		if (dumpLines[i]){
+			ok = ok && /^202[0-9]-[0-1][0-9]-[0-3][0-9] (midi|soir) (A|L|M|P|,)+? (A|L|M|P|X) (.+?)$/.test(dumpLines[i]);
+			//ok = ok && /^202/.test(dumpLines[i]);
+			console.log(dumpLines[i]+" so still ok? "+ok);
+		}
+	}
+	if (ok){
+		setCheckOk();
+	}else{
+		setCheckKo();
+	}
 };
+
+function setDumpCheck(html){
+	document.getElementById("dump-check-status").innerHTML = html;
+};
+function setCheckOk(){setDumpCheck("ok!");};
+function setCheckKo(){setDumpCheck("ko!");};
+function resetCheck(){setDumpCheck("no check");};
+
