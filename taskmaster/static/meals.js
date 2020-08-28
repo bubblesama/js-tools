6158c267e99212a2e5608bceb1cde1d9525ebb47
bubblesama <paul.remignon@gmail.com>
2020-08-28 22:04:30 +0200

    meals: full check with one message, client side

--- taskmaster/static/meals.js
+++ taskmaster/static/meals.js
@@ -19,6 +19,7 @@ document.getElementById("button-set-time-midi").onclick = setTimeAsMidi;
 document.getElementById("button-set-time-soir").onclick = setTimeAsSoir;
 
 //cook
+const POTENTIAL_COOKS = ["A","L","M","P","X"];
 function setCook(cookLetter){document.getElementById("input-cook").value=cookLetter;};
 function setCookAsLise(){setCook("L");};
 function setCookAsMaman(){setCook("M");};
@@ -56,3 +57,50 @@ document.getElementById("button-set-eater-maman").onclick = toggleEaterMaman;
 document.getElementById("button-set-eater-paul").onclick = toggleEaterPaul;
 document.getElementById("button-set-eater-amelie").onclick = toggleEaterAmelie;
 document.getElementById("button-clear-eaters").onclick = clearEaters;
+
+//format control
+var FOOD_REGEX = /^([A-zÀ-ú]|\s)+?$/gi;
+document.getElementById("form-meal").addEventListener('submit', function (event) {
+	var isMealFormValid = true;
+	//control each field
+	//date
+	var dateRawValue = document.getElementById("input-date").value;
+	var parsedDate = moment(dateRawValue,"YYYY-MM-DD",true);
+	if (!parsedDate.isValid()){
+		console.log("#formatControl invalid date:"+dateRawValue);
+		isMealFormValid = false;
+	}
+	//meal
+	var mealRawValue = document.getElementById("input-time").value;
+	if (mealRawValue != "midi" && mealRawValue != "soir"){
+		console.log("#formatControl invalid meal:"+mealRawValue);
+		isMealFormValid = false;
+	}
+	//cook
+	var cookRawValue = document.getElementById("input-cook").value;
+	if (!POTENTIAL_COOKS.includes(cookRawValue)){
+		console.log("#formatControl invalid cook:"+cookRawValue);
+		isMealFormValid = false;
+	}
+	//eaters
+	var eatersRawValue = document.getElementById("input-eaters").value;
+	var eaters = eatersRawValue.split(',');
+	for (var i=0;i<eaters.length;i++){
+		if (!POTENTIAL_EATERS.includes(eaters[i])){
+			console.log("#formatControl invalid eater:"+eaters[i]);
+			isMealFormValid = false;
+		}
+	}
+	//food
+	var foodRawValue = document.getElementById("input-food").value;
+	if (foodRawValue == null || foodRawValue == "" || !foodRawValue.match(FOOD_REGEX)){
+		console.log("#formatControl invalid food:"+foodRawValue);
+		isMealFormValid = false;
+	}
+
+	if (!isMealFormValid){
+		document.getElementById("input-meal-warning").style.display="block";
+		event.preventDefault();
+	}
+});
+
