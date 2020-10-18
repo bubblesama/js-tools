35206dc1fedb0586c7d0975034da3892e42be1f3
bubblesama <paul.remignon@gmail.com>
2020-10-18 18:15:17 +0200

    installor: simple conf file then tmp copy dir

--- installor/installor.js
+++ installor/installor.js
@@ -1,25 +1,41 @@
 //dependances
 var fs = require("fs");
 const scp = require("node-scp");
+var path = require("path");
 
 //récupération des arguments
-process.argv.forEach(function (val, index, array) {
-    console.log(index + ': ' + val);
-});
+var confPath = process.argv[2]; //installator-horoscope.json
+var passphrase = process.argv[3];
+//TODO contrôle des paramètres
 
-//mot de passe
-var passphrase = process.argv[2];
-//clé
-var relativeKeyPath = "./keys/bubble_key_priv.pem"; 
+var conf = JSON.parse(fs.readFileSync(confPath, 'utf8'));
+// preparation du dossier source
+// creation
+var tmpSourceDirPath = conf.source.tmp+"/"+conf.name;
+if (fs.existsSync(tmpSourceDirPath)){
+  console.log(tmpSourceDirPath+" exists: rm then mk");
+  fs.rmdirSync(tmpSourceDirPath,{recursive: true});
+  fs.mkdirSync(tmpSourceDirPath,{recursive: true});
+}else{
+  console.log(tmpSourceDirPath+" does not exist: mk");
+  fs.mkdirSync(tmpSourceDirPath,{recursive: true});
+}
+// copie
+for (var i=0;i<conf.source.files.length;i++){
+  var sourceFile = conf.source.files[i];
+  var destFile = tmpSourceDirPath+"/"+path.basename(conf.source.files[i]);
+  console.log("copy "+sourceFile+" to "+destFile);
+  fs.copyFileSync(sourceFile, destFile);
+}
 
+//envoi
 
 //document de test
 scp({
-  host: "217.182.206.249",
-  port: 22,
+  host: conf.target.host.ip,
+  port: conf.target.host.port,
   username: "bubble",
-  //password: 'password',
-  privateKey: fs.readFileSync(relativeKeyPath),
+  privateKey: fs.readFileSync(conf.target.account.key),
   passphrase: passphrase
 }).then(client => {
   client.uploadFile("./test.txt", "/tmp/test/test.txt")
